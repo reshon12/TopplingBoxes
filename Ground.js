5a98ef1ee83792b75c4ecba2560dd8f351150ed5
reshon12 <69683590+reshon12@users.noreply.github.com>
2020-09-30 19:49:01 +0530

    Add files via upload

--- Ground.js
+++ Ground.js
@@ -1,17 +1,20 @@
 class Ground {
-    constructor(x,y,width,height) {
+    constructor(x, y, width, height) {
       var options = {
-          isStatic: true
+          isStatic:true
       }
-      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.body = Bodies.rectangle(x, y, width, height, options);
       this.width = width;
       this.height = height;
+      
       World.add(world, this.body);
     }
     display(){
       var pos =this.body.position;
       rectMode(CENTER);
-      fill(255);
+      fill("brown");
       rect(pos.x, pos.y, this.width, this.height);
+      
     }
   };
+  
--- box.js
+++ box.js
@@ -2,8 +2,9 @@ class Box {
   constructor(x, y, width, height) {
     var options = {
         'restitution':0.8,
-        'friction':0.3,
-        'density':1.0
+        'friction':1.0,
+        'density':1.0,
+        
     }
     this.body = Bodies.rectangle(x, y, width, height, options);
     this.width = width;
@@ -13,13 +14,16 @@ class Box {
   }
   display(){
     var pos =this.body.position;
-    var angle = this.body.angle;
-    push();
-    translate(pos.x, pos.y);
-    rotate(angle);
+     var angle =this.body.angle;
+     push();
+     translate(pos.x,pos.y);
+     rotate(angle)
     rectMode(CENTER);
+    strokeWeight(4)
+    stroke("green")
     fill(255);
-    rect(0, 0, this.width, this.height);
+    rect(0,0, this.width, this.height);
     pop();
   }
 };
+
